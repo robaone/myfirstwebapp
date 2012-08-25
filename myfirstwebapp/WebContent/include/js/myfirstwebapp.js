ef2297318c6b87e3fcf814307fecda0c5ddce1cb
ansel <ansel@robaone.com>
2012-08-24 23:56:41 -0500

    Adding separate javascript source files for application

--- myfirstwebapp/WebContent/include/js/myfirstwebapp.js
+++ myfirstwebapp/WebContent/include/js/myfirstwebapp.js
@@ -1,3 +1,9 @@
 /**
 The Client side application code
 **/
+
+var appModule = angular.module('myfirstwebapp',[]);
+
+appModule.config(['$routeProvider',function($routeProvider){
+	alert('Hello World');
+}]);
