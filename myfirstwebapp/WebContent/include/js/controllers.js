91dfb78ca4cbe2a3f733f2e567f7fe054f27d3bb
ansel <ansel@robaone.com>
2012-09-19 17:32:15 -0600

    Initial test

---
+++ myfirstwebapp/WebContent/include/js/controllers.js
@@ -0,0 +1,11 @@
+/*
+Controllers
+*/
+
+App.loginCtrl = function($scope){
+	$scope.hi = 'Hello';
+};
+
+App.homeCtrl = function($scope){
+	$scope.hi = $scope.hi + ' 1';
+};
--- myfirstwebapp/WebContent/include/js/myfirstwebapp.js
+++ myfirstwebapp/WebContent/include/js/myfirstwebapp.js
@@ -1,9 +1,17 @@
 /**
 The Client side application code
 **/
+var App = {};
 
 var appModule = angular.module('myfirstwebapp',[]);
 
-appModule.config(['$routeProvider',function($routeProvider){
-	alert('Hello World');
-}]);
+appModule.config(
+		[
+		 	'$routeProvider',
+		 	function($routeProvider){
+		 		$routeProvider.when('', {redirectTo: '/home'});
+		 		$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: App.homeCtrl});
+		 		$routeProvider.otherwise({redirectTo: '/home'});
+		 	}
+		]
+);
